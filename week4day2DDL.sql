e96a5c3bfbb8851ad995f0dfe695578847db06b2
Your Name aliciaxiong <aliciaxiong22@gmail.com>
2023-11-01 15:19:16 -0700

    trying to correct dml theatre section

--- week4day2DDL.sql
+++ week4day2DDL.sql
@@ -9,31 +9,31 @@ CREATE TABLE movie(
 CREATE TABLE customer(
     customer_id SERIAL PRIMARY KEY,
     first_name VARCHAR,
-    last_name VARCHAR
+    last_name VARCHAR,
+    amount_spent VARCHAR
 );
 
 CREATE TABLE tickets(
     ticket_id SERIAL PRIMARY KEY,
-    senior_tickets VARCHAR,
-    adult_tickets VARCHAR,
-    kids_tickets VARCHAR,
-    amount VARCHAR
+    senior_tickets_cost VARCHAR,
+    adult_tickets_cost VARCHAR,
+    kids_tickets_cost VARCHAR,
 );
 
 CREATE TABLE concession(
     concenssion_id SERIAL PRIMARY KEY,
-    drink VARCHAR,
-    food VARCHAR,
-    amount VARCHAR
+    drink_cost VARCHAR,
+    food_cost VARCHAR,
 );
 
 CREATE TABLE theater(
     theater_id SERIAL PRIMARY KEY,
     movie_id INTEGER,
     FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
-    customer_iD INTEGER,
+    customer_id INTEGER,
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-    concenssion_id INTEGER 
-    FOREIGN KEY (concenssion_id REFERENCES concession(concenssion_id)),
-    ticket_id VARCHAR
+    concenssion_id INTEGER,
+    FOREIGN KEY (concenssion_id) REFERENCES concession(concenssion_id),
+    ticket_id VARCHAR,
+    locations VARCHAR
 );
---
+++ week4day2DML.sql
@@ -0,0 +1,87 @@
+INSERT INTO (movie
+    movie_title,
+    rating,
+    genre,
+    movie_length,
+    ) VALUES (
+        'The Nun 2',
+        'R',
+        'Horror',
+        '1 hour 50 minutes'
+    ) (
+        'No Hard Feelings',
+        'R',
+        'Comedy',
+        '1 hour 43 minutes'
+    ) (
+        'Barbie',
+        'PG-13',
+        'Fantasy',
+        '1 hour 54 minutes'
+    ) (
+        'What Happens Later',
+        'R',
+        'Comedy, Romance',
+        '1 hour 45 minutes'
+    ) (
+        'Saving Private Ryan'
+        'R'
+        'Drama',
+        '2 hour 49 minutes'
+    )
+
+INSERT INTO (customer
+    first_name,
+    last_name,
+    amount_spent
+) VALUES (
+    'John',
+    'Mcdonald',
+    '$60.00'
+) (
+    'Christy',
+    'Jackson',
+    '$100.00'
+) (
+    'Melanie',
+    'Savie',
+    '$30.00'
+) (
+    'Harry',
+    'Douglas',
+    '$40.00'
+)
+
+INSERT INTO (
+    senior_tickets,
+    adult_tickets,
+    kids_tickets
+) (VALUES
+    '$15.00',
+    '10.00',
+    '5.00'
+)
+
+INSERT INTO (concession
+    drink_cost,
+    food_cost
+) (VALUES
+    '$5.00',
+    '$10.00',
+    )
+
+INSERT INTO (theater
+    theater_id,
+    movie_id,
+    customer_id,
+    concenssion_id,
+    ticket_id,
+    locations
+) (VALUES
+    theater_id,
+    movie_id,
+    customer_id,
+    concenssion_id
+    ticket_id,
+    locations
+)
